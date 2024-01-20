b077d58287cf9e9110f51fb63b6af1889b8b71ec
carlosspino <carlospino@uma.es>
2024-01-20 18:06:24 +0100

    Upload generator

--- src/dataset/generators/MCF7H.py
+++ src/dataset/generators/MCF7H.py
@@ -1,23 +1,35 @@
-from os import listdir
-from os.path import isfile, join
 import numpy as np
 import networkx as nx
 from src.dataset.generators.base import Generator
 from src.dataset.instances.graph import GraphInstance
-
+import os
+from os import listdir
+from os.path import isfile, join
 class MCF7HGenerator(Generator):
 
     def init(self):
-        self.num_node = self.local_config['parameters'].get('num_nodes', 100)
-        self.num_edges = self.local_config['parameters'].get('num_edges', 500)
-        self.num_graphs = self.local_config['parameters'].get('num_graphs', 8)
 
-        self.adj
+        # Obtén la ruta del directorio actual del script
+        script_dir = os.path.dirname(os.path.abspath(__file__))
 
+        # Construye la ruta al directorio del dataset MCF7H dentro del proyecto Gretel
+        base_path = os.path.join(script_dir, 'data', 'datasets', 'MCF7H')
+
+        #PATH to the MCF7H dataset
+        #We save in variables each path
+        self.edg_file_path = os.path.join(base_path, 'MCF7H_A.txt')
+        self.nod_file_path = os.path.join(base_path, 'MCF7H_graph_indicator.txt')
+        self.grph_file_path = os.path.join(base_path, 'MCF7H_graph_labels.txt')
+        self._nlbls_file_path = os.path.join(base_path, 'MCF7H_node_labels.txt')
+        self._attrib_file_path = os.path.join(base_path, 'MCF7H_node_attributes.txt')
+
+        #Generate dataset
         self.generate_dataset()
 
+        
     def get_num_instances(self):
         return len(self.dataset.instances)
+
 
     def generate_dataset(self):
         for i in range(self.num_instances):
@@ -27,15 +39,3 @@ class MCF7HGenerator(Generator):
             label = 1
             self.dataset.instances.append(GraphInstance(id=i, data=tree, label=label))
             self.context.logger.info("Generated instance with id: " + str(i))
-
-    def join(self, base, cycles):
-        Ab = base
-        A = Ab
-        for cycle in cycles:
-            A_o = cycle
-            t_node = np.random.randint(0, len(Ab))
-            s_node = len(A) + np.random.randint(0, len(A_o))
-            A = np.block([[A, np.zeros((len(A), len(A_o)))], [np.zeros((len(A_o), len(A))), A_o]])
-            A[t_node, s_node] = 1
-            A[s_node, t_node] = 1
-        return A
