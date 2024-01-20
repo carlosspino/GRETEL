aba1a4bce32bba5a619289c36816e9e3169ca69a
Carlos Ramírez <rrscarlos@uma.es>
2024-01-20 16:14:52 +0100

    add

--- src/dataset/generators/MCF7H.py
+++ src/dataset/generators/MCF7H.py
@@ -14,20 +14,6 @@ class MCF7HGenerator(Generator):
     def get_num_instances(self):
         return len(self.dataset.instances)
 
-    def generate_infinity_cycle(self):
-        half_cycle_len = self.infinity_cycle_length // 2
-        g = nx.cycle_graph(half_cycle_len)
-        h = nx.cycle_graph(half_cycle_len)
-        h = nx.relabel_nodes(h, {i: half_cycle_len + i for i in range(max(h.nodes) + 1)})
-        max_nodes_g, max_nodes_h = max(g.nodes), max(h.nodes)
-        common_node = self.infinity_cycle_length
-        g.add_node(common_node)
-        h.add_node(common_node)
-        g.add_edge(max_nodes_g, common_node)
-        h.add_edge(max_nodes_h, common_node)
-        inf_cycle = nx.compose(g, h)
-        return nx.to_numpy_array(inf_cycle)
-
     def generate_dataset(self):
         for i in range(self.num_instances):
             tree = nx.to_numpy_array(nx.random_tree(n=self.num_nodes_per_instance))
